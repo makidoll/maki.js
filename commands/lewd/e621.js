171d19c05540245da319caa13a56537fa7b77a1d
Maki <mxmcube@gmail.com>
2018-06-11 22:42:36 +0100

    some puppy told me to join my args ¯\_(ツ)_/¯

--- commands/lewd/e621.js
+++ commands/lewd/e621.js
@@ -1,14 +1,17 @@
 var request = require("request");
 
 module.exports = function(msg) {
+	let splitTags = msg.content.split(" ");
+
 	if (!msg.channel.nsfw && !(msg.channel.type === "dm")) { msg.channel.send("You can only use this command in **NSFW** channels."); msg.channel.stopTyping(); return; }
-	if (msg.content.split(" ")[1] == undefined) { msg.channel.send("Incorrect syntax! Try **"+global.prefix+"lewde621 (tag)**"); msg.channel.stopTyping(); return; }
+	if (splitTags[1] == undefined) { msg.channel.send("Incorrect syntax! Try **"+global.prefix+"lewde621 (tag)**"); msg.channel.stopTyping(); return; }
 
-	let tag = msg.content.split(" ")[1].toLowerCase();
+	splitTags.shift();
+	tags = splitTags.join(",");
+
 	msg.channel.startTyping();
-
 	request({
-		url: "https://e621.net/post/index.json?tags="+tag,
+		url: "https://e621.net/post/index.json?tags="+tags,
 		headers: {
 			"User-Agent": "Maki.js"
 		}
@@ -22,7 +25,7 @@ module.exports = function(msg) {
 			//msg.channel.send("**This was rated with " + json["posts"]["post"][post_num]["score"] + " points and the tags are:**\n`" + json["posts"]["post"][post_num]["tags"] + "`\nhttp:" + json["posts"]["post"][post_num]["file_url"]);
 			msg.channel.send("This post by **"+json[post_num]["author"]+"** was rated with **" + json[post_num]["score"] + " points**:\n" + json[post_num]["file_url"]);
 		} catch(err) {
-			msg.channel.send("Nothing exists with **" + tag + "**!");
+			msg.channel.send("Nothing exists with **" + tags + "**!");
 		}  	
 		msg.channel.stopTyping();
 	});
